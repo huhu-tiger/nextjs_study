cea9f5ed8f781543ba4e9f59ca61945bb10d4472
tao.jun <tao.jun@neolink.com>
2025-09-15 18:58:35 +0800

    111

--- ts_study/src/泛型.ts
+++ ts_study/src/泛型.ts
@@ -1,76 +1,97 @@
 // 泛型可以 用于 函数 类 接口 类型别名
 // 无法确定泛型类型 可以 使用 泛型约束
 
-const createArr = <T>(times: T, val: T[]): T[] => {
-    let abc = val.map(item => {
-        return item;
-    });
-    return abc
+
+// 泛型函数
+
+function createArr<T>(times: T, val: T[]): T[] {
+     val.push(times)
+     return val
 }
 
-const arr1 = createArr("q", ['a'])
+console.log(createArr<string>("1", ["1","2"])) // [ '1', '2', '1' ]
+
+// // keyof 约束
+function showobj<T,K extends keyof T>(obj:T,val:K):T[K]{
+    return obj[val]
+}
+const objex11= {
+    "a":"b"
+}
+console.log(`showobj: ${showobj(objex11,"a")}`) // "b"
 
 
+const showObj11=<T,K extends keyof T>(obj :T,key :K):T[K] =>{
+    return obj[key]
+}
+
+console.log(`showObj11: ${showObj11(objex11,"a")}`) // "b"
 
 // 泛型约束
 // 1. 泛型约束 可以 约束 泛型 的类型
 interface ILength{
     length:number
 }
-
+// // 扩展约束， T 必须要有length 属性
 const getLength = <T extends ILength>(arg:T):number=>{
     return arg.length
 }
 
-getLength("123")
-getLength({length:123})
+console.log(getLength("123")) // 3
+console.log(getLength({length:123})) // 123
 
-
-// 2. 泛型约束 
-const fnn1 = <T ,key extends keyof T>(arg:T,key1:key):T[key]=>{
-    return arg[key1]
-}
-
-let obj111  = {
-    name:"123",
-    age:123
-}
-
-console.log(fnn1(obj111,"name"))
-console.log(fnn1("dddd","length")) // 4, 字符串 有 length 属性
-console.log(fnn1(["abc","bn"],1))    //  bn, 1 代表索引位置
 
 // 写辅助函数
 // 值的交换,T,U 名字自定义
-const swap = <T, U>(tuple: [T, U]): [U, T] => {
-    return [tuple[1], tuple[0]]
+const swap =<T,U>(arr1:[T,U]):[U,T]=>{
+    return [arr1[1],arr1[0]]
 }
 
-const swapArr = swap(['a', 1])
+console.log(swap(["1",2])) // [2,"1"]
+
+// 函数形参为函数
+
+
+const fanxing1=<T,U>(arr:T[],mapper:(item:T)=>T):T[]=>{
+    return arr.map(mapper)
+}
+
+console.log(fanxing1(["1","2"],(item)=>{return `cook ${item}`})) // ["cook 1","cook 2"]
 
 // 接口泛型
 
-interface IArr<T>{
-    a:T
-    arr:T[]
+interface I2<T> {
+    age:T
 }
 
-let arr111:IArr<number> = {
-    a:1,
-    arr:[1,2,3]
+interface I1<S,T> extends I2<T> {
+    name:S
 }
+
+
+const faxing2=(data:I1<string,number>):I1<string,number>=>{
+    return {name:data.name,age:data.age+1}
+}
+
+console.log(faxing2({name:"aaa",age:18})) // {name:"aaa",age:19}
 
 // 泛型类
 
 class MyArray<T>{
     private arr:T[] = []
-    add(item:T){
+
+    set(item:T){
         this.arr.push(item)
     }
+    get(): T[]{
+        return this.arr
+    }
+    
     
 }
 const m = new MyArray<number>()
-
+m.set(1)
+console.log(m.get()) // [1]
 
 // 泛型工具类型
 /*
